710b94b4ced4789a7bdfe5eaf9f47b39d139e3ad
Frederik Agerkvist <s153565@student.dtu.dk>
2016-06-06 12:45:29 +0200

    davs

--- app/src/main/java/com/example/stephan/quizz/login.java
+++ app/src/main/java/com/example/stephan/quizz/login.java
@@ -2,12 +2,21 @@ package com.example.stephan.quizz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button blogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        blogin = (Button) findViewById(R.id.blogin);
     }
 }
--- app/src/main/java/com/example/stephan/quizz/register.java
+++ app/src/main/java/com/example/stephan/quizz/register.java
@@ -2,12 +2,25 @@ package com.example.stephan.quizz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class register extends AppCompatActivity {
+
+    private EditText etUsername,etPassword, atPassword;
+    private Button bregister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        atPassword = (EditText) findViewById(R.id.atPassword);
+        bregister = (Button) findViewById(R.id.bregister);
+
     }
+
+
 }
