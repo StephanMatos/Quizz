87171fa65e910eeecb6aaee21a6ea7baf1c8b53c
Mwthorn <mwthorn@gmail.com>
2016-06-06 15:26:13 +0200

    commit dat R

--- app/src/main/java/com/example/stephan/quizz/LoginActivity.java
+++ app/src/main/java/com/example/stephan/quizz/LoginActivity.java
@@ -13,7 +13,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_Login);
+        setContentView(R.layout.activity_login);
 
         etUsername = (EditText) findViewById(R.id.etUsername);
         etPassword = (EditText) findViewById(R.id.etPassword);
