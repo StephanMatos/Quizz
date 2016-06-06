308e03cc72961392b93b88dd8d02dbf021e3e7c6
Stephan Matos <Stephanmatos@gmail.com>
2016-06-06 11:16:09 +0200

    Hej

--- app/src/main/java/Login.java
+++ app/src/main/java/Login.java
@@ -1,5 +1,24 @@
 /**
  * Created by Stephan on 03-06-2016.
  */
+import java.awt.*;
+
 public class Login {
+
+    public Login(){
+
+    }
+
+
+
+
+    public login(String username, String password){
+
+        String user = username;
+        String pass = password;
+
+
+
+    }
+
 }
