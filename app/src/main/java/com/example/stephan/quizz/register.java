d80bb6310255457bf3956293f09234f7d3912481
Frederik Agerkvist <s153565@student.dtu.dk>
2016-06-06 11:31:24 +0200

    davs

---
+++ app/src/main/java/com/example/stephan/quizz/register.java
@@ -0,0 +1,13 @@
+package com.example.stephan.quizz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
