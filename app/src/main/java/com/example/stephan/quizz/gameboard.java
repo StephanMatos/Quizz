8f2dd8fa6996da6a23c9aec24f525114a8eb1721
Stephan Matos <Stephanmatos@gmail.com>
2016-06-13 10:37:32 +0200

    Last update onsdag

--- app/src/main/java/com/example/stephan/quizz/gameboard.java
+++ app/src/main/java/com/example/stephan/quizz/gameboard.java
@@ -3,6 +3,8 @@ package com.example.stephan.quizz;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.graphics.Color;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +13,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 @TargetApi(Build.VERSION_CODES.GINGERBREAD)
@@ -20,6 +27,13 @@ import java.util.concurrent.TimeUnit;
 public class gameboard extends AppCompatActivity {
 
     TextView etTimer;
+    private BufferedReader bir;
+    private PrintWriter pw;
+    private String answer1,answer2,answer3,answer4,question;
+    private ArrayList<String> index;
+    private Button bAnswer1,bAnswer2,bAnswer3,bAnswer4;
+    private TextView etQuestion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +44,11 @@ public class gameboard extends AppCompatActivity {
         final TextView Player2 = (TextView) findViewById(R.id.Player2);
         final TextView Score1 = (TextView) findViewById(R.id.score1);
         final TextView Score2 = (TextView) findViewById(R.id.score2);
-        final TextView etQuistion = (TextView) findViewById(R.id.etQuestion);
-        final Button bAnswer1 = (Button) findViewById(R.id.bAnswer1);
-        final Button bAnswer2 = (Button) findViewById(R.id.bAnswer2);
-        final Button bAnswer3 = (Button) findViewById(R.id.bAnswer3);
-        final Button bAnswer4 = (Button) findViewById(R.id.bAnswer4);
+        etQuestion = (TextView) findViewById(R.id.etQuestion);
+        bAnswer1 = (Button) findViewById(R.id.bAnswer1);
+        bAnswer2 = (Button) findViewById(R.id.bAnswer2);
+        bAnswer3 = (Button) findViewById(R.id.bAnswer3);
+        bAnswer4 = (Button) findViewById(R.id.bAnswer4);
         etTimer = (TextView) findViewById(R.id.etTimer);
 
         etTimer.setText("00:30");
@@ -48,6 +62,28 @@ public class gameboard extends AppCompatActivity {
                 timer.start();
             }
         });
+        bAnswer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.start();
+            }
+        });
+        bAnswer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.start();
+            }
+        });
+        bAnswer4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.start();
+            }
+        });
+
+       new Load().execute();
+
+
 
     }
 
@@ -98,5 +134,52 @@ public class gameboard extends AppCompatActivity {
         etTimer.setText("Better be faster next time!");
 
         }
+    }
+
+
+    public class Load extends AsyncTask<Void,String,Void>{
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            Network net = Network.getInstance();
+            BufferedReader bir = net.getBir();
+            PrintWriter pw = net.getPw();
+
+            return null;
+        }
+        protected void onPostExecute(){
+
+            String question = null;
+            String answer1 = null;
+            String answer2 = null;
+            String answer3 = null;
+            String answer4 = null;
+            try {
+
+                question = bir.readLine();
+                answer1 = bir.readLine();
+                answer2 = bir.readLine();
+                answer3 = bir.readLine();
+                answer4 = bir.readLine();
+
+            }catch (IOException e){
+
+            }
+
+            bAnswer1.setText(answer1);
+            bAnswer2.setText(answer2);
+            bAnswer3.setText(answer3);
+            bAnswer4.setText(answer4);
+            etQuestion.setText(question);
+
+        }
+
+
+
+
+
+
     }
 }
